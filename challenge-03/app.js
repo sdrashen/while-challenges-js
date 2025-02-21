9d81e1499bcd316e65c783f8250c3f80edded468
drashen <sdrashe@gmail.com>
2025-02-21 16:09:38 -0300

    feat: sum positive numbers using while

--- challenge-03/app.js
+++ challenge-03/app.js
@@ -1,2 +1,12 @@
+//Ask the user to enter numbers. Keep summing these numbers until the user enters a negative number. When that happens, display the total sum.
 console.log('Boas-vindas!')
 
+let number = Number(prompt('Insira um número:'));
+let total = 0;
+
+while ( number >= 0) {
+  total += number;
+  number = Number(prompt('Insira outro número:'));
+}
+
+console.log(`A soma total dos números inseridos é: ${total}`);
