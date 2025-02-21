81deaa846b0bb2e40bcf17f72057cac9a710621d
drashen <sdrashe@gmail.com>
2025-02-21 17:55:31 -0300

    feat: validate password using while loop

--- challenge-04/app.js
+++ challenge-04/app.js
@@ -1,1 +1,11 @@
 console.log('Boas-vindas!')
+
+let senha = 1234;
+let numero = Number(prompt('Insira uma senha de quatro números em sequência e sem repetir os números:'));
+
+while ( numero != senha ) {
+  alert('Tente novamente.');
+  numero = Number(prompt('Insira uma senha de quatro números em sequência e sem repetir os números:'));
+}
+
+alert('Acesso concedido!');
