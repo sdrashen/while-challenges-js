4947c82d907ec64c5220e148ca0b44159a7a2af7
drashen <sdrashe@gmail.com>
2025-02-21 15:43:38 -0300

    feat: create decreasing list while

--- challenge-02/app.js
+++ challenge-02/app.js
@@ -1,2 +1,9 @@
+//Create a program that asks the user for a number and displays a countdown to 0.
 console.log('Boas-vindas!')
 
+let number = Number(prompt('Insira qulaquer número maior que nove:'));
+
+while (number > 0) {
+    console.log(number)
+    number--;
+}
