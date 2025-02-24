41e079c6a5ffc8140282b8d757b5a77a3522cba3
drashen <sdrashe@gmail.com>
2025-02-24 15:11:18 -0300

    feat: challenge 07 - use multiplication table

--- challenge-07/app.js
+++ challenge-07/app.js
@@ -1,1 +1,11 @@
-console.log('Boas-vindas!')
+//Ask the user to enter a number and display its multiplication table from 1 to 10.
+
+let numero = Number(prompt("Digite um número para ver a tabuada:"));
+let multiplicador = 1;
+
+while (multiplicador <= 10) {
+  alert(`${numero} x ${multiplicador} = ${numero * multiplicador}`);
+  multiplicador++;
+}
+
+alert("Tabuada concluída!");
