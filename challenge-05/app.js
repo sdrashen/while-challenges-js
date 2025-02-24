4f439633053eb21e313cc88e49de781a3bec25d6
drashen <sdrashe@gmail.com>
2025-02-24 14:17:26 -0300

    feat: challenge 05 - number higher than zero

--- challenge-05/app.js
+++ challenge-05/app.js
@@ -1,1 +1,15 @@
+//Ask the user to enter numbers until they enter 0. At the end, display the highest number entered.
 console.log('Boas-vindas!')
+
+let numero = Number(prompt('Insira um número qualquer:'));
+let maiorNumero = numero;
+
+while ( numero !== 0) {
+  numero = Number(prompt('Insira um número qualquer:'));
+
+  if (numero > maiorNumero && numero !== 0) {
+    maiorNumero = numero;
+  }
+}
+
+alert(`O maior número inserido antes do '0' foi: ${maiorNumero}`);
