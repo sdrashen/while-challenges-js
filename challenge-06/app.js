112ed9c62cf5d6690e02248ea965341e2a26f876
drashen <sdrashe@gmail.com>
2025-02-24 14:56:01 -0300

    feat: challenge 06 - sum numbers and ask if continue sum

--- challenge-06/app.js
+++ challenge-06/app.js
@@ -1,1 +1,19 @@
-console.log('Boas-vindas!')
+//Make a program using a `while` loop that allows the user to perform addition calculations as long as they want.
+console.log('Boas-vindas!');
+
+let continuar = true;
+
+while (continuar) {
+  let numero1 = Number(prompt('Insira um número:'));
+  let numero2 = Number(prompt('Insira outro número:'));
+
+  let soma = numero1 + numero2;
+  alert(`O resultado da soma é: ${soma}`);
+
+  let resposta = prompt('Deseja realizar outra soma? Digite "1" para Sim ou "0" para Parar.');
+
+  if (resposta !== "1") {
+    continuar = false;
+    alert('Programa finalizado.');
+  }
+}
